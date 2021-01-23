65f0f95fc1490dcaa9c0cdf78f21dfb69965fa84
indi06 <isabelsuh@hunterschools.org>
2021-01-23 00:12:19 +0000

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import os
+import time
+
+
 print("MadLibs Final Project")
 print("By Indigo Suh")
 print()
 answer = input("Ready to play? Type yes or no:  ")
 if answer == "yes" or answer == "Yes" or answer == "yes." or answer == "yes." or answer == "Yes!" or answer == "yes!" :
-  import random.file
+  import random
+  madlib=random.randint(1, 2)
+  if madlib==1:
+    import madlib1
+  elif madlib==2:
+    import madlib2
 else:
     print("Okay! Have a nice day.")
     exit()
