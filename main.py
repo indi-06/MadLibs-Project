c8588f62a08d720527355bee0c2266c8a43d8218
indi06 <isabelsuh@hunterschools.org>
2021-01-23 00:03:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("MadLibs Final Project")
+print("By Indigo Suh")
+print()
+answer = input("Ready to play? Type yes or no:  ")
+if answer == "yes" or answer == "Yes" or answer == "yes." or answer == "yes." or answer == "Yes!" or answer == "yes!" :
+  import random.file
+else:
+    print("Okay! Have a nice day.")
+    exit()
+
+
